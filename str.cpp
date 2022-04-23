def9d39421a68b7d30d7bd29c7947c7afc6c79de
lijunjun <gionchat@gmail.com>
2022-04-23 10:10:52 +0800

    init project

---
+++ str.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+         
+ string str1 = "qwert";
+ string str2 = "qwert";   
+ int result = strcmp(str1,str2);
+ cout<<result<<endl;
+             
+  return 0; 
+}
